8b9007b86c91bcd73da4a400db1162672718126f
Mani Kumar <mk0872191152@gmail.com>
2020-03-24 17:31:06 +0530

    add pagination

--- app/config.py
+++ app/config.py
@@ -14,3 +14,5 @@ class Config(object):
         MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
         MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
         ADMINS = ['mani.kumar8721@gmail.com']
+
+        POSTS_PER_PAGE = 25
